720a6bb2b1fccb4f6b11a5065e714b6715f280bd
curtisk808 <gbaamk@gmail.com>
2019-11-29 17:46:57 +0000

    Added Dockerfile

--- os/generator.py
+++ os/generator.py
@@ -1,12 +1,7 @@
 from __future__ import print_function
 import random
 
-buzz = ('continuous testing', 'continuous integration',
-    'continuous deployment', 'continuous improvement', 'devops')
-adjectives = ('complete', 'modern', 'self-service', 'integrated', 'end-to-end')
-adverbs = ('remarkably', 'enormously', 'substantially', 'significantly',
-    'seriously')
-verbs = ('accelerates', 'improves', 'enhances', 'revamps', 'boosts')
+buzz = ('1', '2', '3', '4', '5', '6')
 
 def sample(l, n = 1):
     result = random.sample(l, n)
@@ -16,8 +11,7 @@ def sample(l, n = 1):
 
 def generate_buzz():
     buzz_terms = sample(buzz, 2)
-    phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
-        sample(verbs), buzz_terms[1]])
+    phrase = sample(buzz)
     return phrase.title()
 
 if __name__ == "__main__":
